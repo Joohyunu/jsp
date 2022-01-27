64e5d09036bd5c528147582d909c0f5a7ecb04a0
eninen <eninen@59.16.170.144>
2022-01-27 10:19:26 +0900

    테스트

---
+++ JspGit/src/_1/Index3.java
@@ -0,0 +1,25 @@
+package _1;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index2")
+public class Index3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Index3() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("NewFile2.jsp");
+		dispatcher.forward(request, response);
+	}
+}
